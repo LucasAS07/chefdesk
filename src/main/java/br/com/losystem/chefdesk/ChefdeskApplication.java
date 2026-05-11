a62e0112941fbf89311ed4ca7cc29df310d073a0
Lucas Rodrigues de Oliveira <lucas-oliveira25@outlook.com>
2026-05-11 13:53:38 -0300

    Subindo projeto para GitHub

---
+++ src/main/java/br/com/losystem/chefdesk/ChefdeskApplication.java
@@ -0,0 +1,13 @@
+package br.com.losystem.chefdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/losystem/chefdesk/ChefdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.losystem.chefdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
